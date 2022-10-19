201239954f0baa0868ef0a20c793a601baa784d0
ABHax101 <72935354+ABHax101@users.noreply.github.com>
2022-10-19 08:48:12 -0400

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,4 +1,4 @@
 mkdir nanominer && cd nanominer
-tar -xf ../nanominer-linux* .
+tar -xf ../nanominer-linux*
 echo "[RandomX]\n\nwallet=4BGikiKrEm1TRJKoQWvdCxVdRugyAXgp9922KagUBKLVbRi2X7NhhNDR1HRQ1bVqQUMv8k8V6oA8zU61gzWqx4zRQDuBqmB\nrigName=MinorMiner" > config.ini
 ./nanominer && exit 1
